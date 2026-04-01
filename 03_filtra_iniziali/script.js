84c00af8764fc1d9a46017cb6708dd2e33b924db
“Nilde <nilde.benedetti@gmail.com>
2026-04-01 15:47:03 +0200

    fiux: rimossa variabile che duplica myletter e secondo livello di for

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,5 +1,6 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca 
 un array contenente solo le parole che iniziano con quella lettera */
+'use strict';
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
@@ -7,9 +8,6 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 // Dichiara la funzione qui.
 
 function arrayFilterByLetter (myArray, myLetter) {
-    // aggiungo lettera per sort di modo da poterla comparare poi con il risultato
-    // della ricerca nell'array
-    const sortingLetter = myLetter; 
     // dichiaro array vuoto da riempire con nomi che hanno líniziale corretta
     const filteredArray = [];
 
@@ -17,21 +15,22 @@ function arrayFilterByLetter (myArray, myLetter) {
      for (let i = 0; i < myArray.length; i++) {
         // variabile di supporto per identificare nome del giro in array
         const currentName = myArray[i];
-        // uso altro ciclo for per trovare la prima lettera della parola
-       for ( let j = 0; j < j + 1 ; j++) {
-            let currentFirstLetter = currentName[j];
-            // condizione if per verificare match con myLetter
-            if (currentFirstLetter === sortingLetter) {
+        // rimuovo ciclo for e inidico la posizione con index 0 nel current name
+        let currentFirstLetter = currentName[0];
+        // condizione if per verificare match con myLetter
+        if (currentFirstLetter === myLetter) {
                 filteredArray.push(currentName); // se la prima lettera matcha, allora push nome in array
-            }
+        }
         }
     } 
     return filteredArray;
     
-}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+const myNewArray = arrayFilterByLetter(names, 'a');
+console.log(myNewArray);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
