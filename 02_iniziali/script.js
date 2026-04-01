cc62bf8e5af0e59e07ff7675c25f184357b3ca54
“Nilde <nilde.benedetti@gmail.com>
2026-04-01 15:07:12 +0200

    add: function per pescare iniziali in array names + test - 02_iniziali/script.js

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,25 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+// devo creare un ciclo for per scorrere i nomi
+// per ogni nome, deve pescare la lettera con index 0
+
+function pescaIniziali (arrayNomi) {
+    const arrayLettere = [];
+    for (let i = 0; i < arrayNomi.length; i++) {
+        const currentNome = arrayNomi[i];
+       for ( let j = 0; j < currentNome.length; j++) {
+            arrayLettere.push(currentNome[j]);
+            break;
+        }
+    } 
+    return arrayLettere;
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const firtstLetters = pescaIniziali(names);
+console.log(firtstLetters);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
