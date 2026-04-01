032c2d4c9b2c10acf9594bd9c41ccc816b99b777
“Nilde <nilde.benedetti@gmail.com>
2026-04-01 15:50:28 +0200

    test: rimuovo da funzione e testo codice in script.js (funziona!)

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -7,6 +7,8 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+/*
+
 function arrayFilterByLetter (myArray, myLetter) {
     // dichiaro array vuoto da riempire con nomi che hanno líniziale corretta
     const filteredArray = [];
@@ -30,7 +32,24 @@ function arrayFilterByLetter (myArray, myLetter) {
 // Invoca la funzione qui e stampa il risultato in console
 
 const myNewArray = arrayFilterByLetter(names, 'a');
-console.log(myNewArray);
+console.log(myNewArray); */
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+
+// dichiaro array vuoto da riempire con nomi che hanno líniziale corretta
+    const filteredArray = [];
+
+    // uso ciclo for per scorrere l'array con i nomi
+     for (let i = 0; i < names.length; i++) {
+        // variabile di supporto per identificare nome del giro in array
+        const currentName = names[i];
+        // rimuovo ciclo for e inidico la posizione con index 0 nel current name
+        let currentFirstLetter = currentName[0];
+        // condizione if per verificare match con myLetter
+        if (currentFirstLetter === 'A') {
+                filteredArray.push(currentName); // se la prima lettera matcha, allora push nome in array
+        }
+        }
+
+        console.log(filteredArray);
